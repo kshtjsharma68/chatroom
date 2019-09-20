217b4086f651c6db878e57735b5e1d89d6779f28
kshtjsharma68 <kshtjsharma86@gmail.com>
2019-09-20 21:55:54 +0530

    basic setup

---
+++ server/server.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+var server = http.createServer((request, response) {
+    var filePath = false;
+
+    if (request.url == '/') {
+        filePath = 'lib/index.html';
+    } else {
+        filePath = 'lib' + request.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function() {
+    console.log('Server listening on 3000');
+});
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: Resource not found');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200,{"content-type": MimeType.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache(absPath)) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
+}
